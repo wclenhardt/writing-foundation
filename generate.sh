af46c623367874b61f68d1b2bf0bd4b8203ad2e6
Karl Benedict <kbene@unm.edu>
2016-10-28 08:41:56 -0600

    Initial commit of draft foundation paper

---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+pandoc -s -S --normalize --bibliography \
+./agile_data_curation.bib \
+--csl ./elsevier-harvard.csl \
+-f markdown -t docx \
+-o AgileCuration-Foundation.docx AgileCuration-Foundation.txt
+
+pandoc -s -S --normalize --bibliography \
+./agile_data_curation.bib \
+--csl ./elsevier-harvard.csl \
+-f markdown \
+-o AgileCuration-Foundation.pdf AgileCuration-Foundation.txt
+
+
